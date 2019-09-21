cf29e2b08c645d36c99910d6cfaaf456cd5cfdcf
Kye Williams <kwilliams154517@troy.edu>
2019-09-21 15:14:39 -0700

    Super_1

---
+++ superheros.py
@@ -0,0 +1,25 @@
+import random
+
+class Ability:
+	def __init__(self, name, attack_strength):
+		'''Create Instance Variables:
+		name:String
+		max_damage: Integer
+		'''
+		self.name = name
+		self.attack_strength = attack_strength
+		# TODO: Instantiate the variables listed in the docstring with then
+	 	# values passed in
+		pass
+	def attack(self):
+  	  ''' Return a value between 0 and the value set by self.max_damage.'''
+		# TODO: Use random.randint(a, b) to select a random attack value.
+		# Return an attack value between 0 and the full attack.
+		# Hint: The constructor initializes the maximum attack value.
+		#pass
+if __name__ == "__main__":
+    # If you run this file from the terminal
+    # this block is executed.
+    ability = Ability("Debugging Ability", 20)
+    print(ability.name)
+    print(ability.attack())
