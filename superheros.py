facc9acc542bac035f82587b3383f3ec9d56fae6
Kye Williams <kwilliams154517@troy.edu>
2019-09-21 16:22:57 -0700

    Superhero_2

--- superheros.py
+++ superheros.py
@@ -8,18 +8,58 @@ class Ability:
 		'''
 		self.name = name
 		self.attack_strength = attack_strength
-		# TODO: Instantiate the variables listed in the docstring with then
-	 	# values passed in
-		pass
+
 	def attack(self):
-  	  ''' Return a value between 0 and the value set by self.max_damage.'''
-		# TODO: Use random.randint(a, b) to select a random attack value.
-		# Return an attack value between 0 and the full attack.
-		# Hint: The constructor initializes the maximum attack value.
-		#pass
-if __name__ == "__main__":
-    # If you run this file from the terminal
-    # this block is executed.
-    ability = Ability("Debugging Ability", 20)
-    print(ability.name)
-    print(ability.attack())
+		''' Return a value between 0 and the value set by self.max_damage.'''
+		return random.randint(0, self.attack_strength)
+class Armor:
+	def __init__(self, name, max_block):
+		'''Instantiate instance properties.
+			name: String
+			max_block: Integer
+		'''
+		self.name = name
+		self.max_block = max_block
+
+	def block(self):
+		return random.randint(0, self.max_block)
+
+class Hero:
+	def __init__(self, name, starting_health=100):
+		'''Instance properties:
+			abilities: List
+			armors: List
+			name: String
+			starting_health: Integer
+			current_health: Integer
+		'''
+		self.name = name
+		self.starting_health = starting_health
+		self.abilities = []
+		self.armors = []
+
+	def add_ability(self, ability):
+		''' Add ability to abilities list '''
+		self.abilities.append(ability)
+
+	def attack(self):
+		'''Calculate the total damage from all ability attacks.
+			return: total:Int
+		'''
+		total = 0
+		for ability in self.abilities:
+			total += ability.attack()
+		return total
+	def add_armor(self, armor):
+		'''Add armor to self.armors
+		Armor: Armor Object
+		'''
+		self.armors.append(armor)
+
+	def defend(self, damage_amt):
+		'''Runs `block` method on each armor.
+		Returns sum of all blocks
+		'''
+  
+	# TODO: This method should run the block method on each armor in self.armors
+
